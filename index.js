9ec5d7c9bdb36f8e5846ccc7ae4efbc6c6c19edd
Darietta03 <dsnegireva2017@gmail.com>
2023-02-15 22:24:41 +0200

    changes15.02.23

--- index.js
+++ index.js
@@ -3,23 +3,20 @@ let btn = document.querySelector('.searchBtn');
 let modal = document.querySelector('.modal');
 let search =document.querySelector('.search');
 let close = document.querySelector('.closeBtn');
-
+let container = document.querySelector('.container');
+let weather = document.querySelector('.weather');
 
 
 
 btn.addEventListener('click', () => {
- if(city.value === 0) {
-
-
-
-
-
-
-
-
- } else {
+ if(city.value.length === 0) {
     modal.style.display = 'block';
     search.style.display = 'none';
+
+ } else {
+    container.style.height = '600px';
+    weather.style.display = 'flex';
+
 
  }
 })
